613352571941207b38fccd7921d16c98e875e0b3
zxBCN Romero,Jorge (IT EDS) EXTERNAL <jorge.romero.ext@boehringer-ingelheim.com>
2021-03-18 19:21:15 +0100

    Merge pull request #12 in EDPC/edpc-quickstarters-demo from feature/little-installation-script to master
    Squashed commit of the following:
    
    commit a4b993e46c25bacdc4d0022651cf7f6d61c7eb02
    Merge: 18f7da6 3d1d290
    Author: zxBCN Romero,Jorge (IT EDS) EXTERNAL <jorge.romero.ext@boehringer-ingelheim.com>
    Date:   Thu Mar 18 19:20:36 2021 +0100
    
        Merge branch 'master' of https://bitbucket.biscrum.com/scm/edpc/edpc-quickstarters-demo into feature/little-installation-script
    
    commit 18f7da6bc5bb2139530449149d6e2ba8bdcc8394
    Author: zxBCN Romero,Jorge (IT EDS) EXTERNAL <jorge.romero.ext@boehringer-ingelheim.com>
    Date:   Thu Mar 18 14:02:24 2021 +0100
    
        Change script path
    
    commit d7530e394e5c11f7ebdb25be28f88a5d2268562a
    Author: zxBCN Romero,Jorge (IT EDS) EXTERNAL <jorge.romero.ext@boehringer-ingelheim.com>
    Date:   Wed Mar 17 19:29:46 2021 +0100
    
        little script to install the QS

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # edpc-quickstarters-demo
 
 Quickstarters used for provisioning EDP demo applications
-
+# Installing quickstarters
+Having a EDPbox created with the ODS->EDP box installation scripts you can install the Quickstarters of the SockShopDemo with the script provided
+```sh
+  $ install_sock_shop_qs.sh
+```
 ## Running the release manager
 You can find a sample metadata.yaml for the release-manager in the sample folder. 
 To make it work, change all the occurrence of *sock* to the id of your jira project in that file.
-
-
---
+++ scripts/install_sock_shop_qs.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+set -eu
+
+ODS_CORE_HOME='/home/openshift/opendevstack/ods-core/'
+ODS_CONFIGURATION_HOME='/home/openshift/opendevstack/ods-configuration/'
+SSDQS="
+    # sock-shop-demo quickstarters
+    jenkinspipeline.quickstarter.demo-app-sockshop-payment.desc=Demo App - SockShop Payment
+    jenkinspipeline.quickstarter.demo-app-sockshop-payment.repo=ods-quickstarters
+
+    jenkinspipeline.quickstarter.demo-app-sockshop-shipping.desc=Demo App - SockShop Shipping
+    jenkinspipeline.quickstarter.demo-app-sockshop-shipping.repo=ods-quickstarters
+
+    jenkinspipeline.quickstarter.demo-app-sockshop-loadgen.desc=Demo App - SockShop Load / Integration Tests
+    jenkinspipeline.quickstarter.demo-app-sockshop-loadgen.repo=ods-quickstarters
+    
+    jenkinspipeline.quickstarter.demo-app-sockshop-front-end.desc=Demo App - SockShop Front-end
+    jenkinspipeline.quickstarter.demo-app-sockshop-front-end.repo=ods-quickstarters
+
+    jenkinspipeline.quickstarter.demo-app-sockshop-carts.desc=Demo App - SockShop Carts
+    jenkinspipeline.quickstarter.demo-app-sockshop-carts.repo=ods-quickstarters
+
+    jenkinspipeline.quickstarter.demo-app-sockshop-catalogue.desc=Demo App - SockShop Catalogue
+    jenkinspipeline.quickstarter.demo-app-sockshop-catalogue.repo=ods-quickstarters
+
+    jenkinspipeline.quickstarter.demo-app-sockshop-orders.desc=Demo App - SockShop Orders
+    jenkinspipeline.quickstarter.demo-app-sockshop-orders.repo=ods-quickstarters
+
+    jenkinspipeline.quickstarter.demo-app-sockshop-queue-master.desc=Demo App - SockShop Queue Master
+    jenkinspipeline.quickstarter.demo-app-sockshop-queue-master.repo=ods-quickstarters
+
+    jenkinspipeline.quickstarter.demo-app-sockshop-user.desc=Demo App - SockShop User
+    jenkinspipeline.quickstarter.demo-app-sockshop-user.repo=ods-quickstarters
+
+    jenkinspipeline.quickstarter.demo-app-sockshop-tests.desc=Demo App - SockShop E2E Tests
+    jenkinspipeline.quickstarter.demo-app-sockshop-tests.repo=ods-quickstarters
+"
+function add_sock_shop_demo_qs() {
+
+  echo "Starting EDP Project Type configuration in ProvApp"
+  echo "...some debug information first"
+  oc login -u developer -p password
+  oc whoami
+  oc project ods
+
+  CM=$(oc get -o yaml cm/additional-templates.properties)
+  echo "${CM//properties: |/properties: |$SSDQS}" > cm.yaml
+
+  echo "... Apply configmap change"
+  oc apply -f cm.yaml
+  
+  echo "...restart ProvApp to reload new configuration"
+
+  oc rollout latest dc/ods-provisioning-app
+}
+
+function add_qs_to_bitbucket() {
+  scriptPath=$PWD
+  pushd /tmp
+  
+  git clone ssh://git@bitbucket.odsbox.lan:7999/opendevstack/ods-quickstarters.git
+  cd ods-quickstarters
+  git checkout 3.x 
+  cp -R $scriptPath/../edpc-quickstarters-demo/demo* /tmp/ods-quickstarters
+  git add .
+  git commit -m "Sock Shop Demo QS"
+  git push
+  rm -Rf /tmp/ods-quickstarters
+  popd
+}
+add_sock_shop_demo_qs
+add_qs_to_bitbucket
+
+
+
